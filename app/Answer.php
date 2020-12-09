b4918248d5bceb73852cb8ab1584f8fe1b9f8a06
HuyNguyen206 <nguyenlehuyuit@gmail.com>
2020-12-09 18:13:38 +0700

    Allow to delete best answer along with updating best_answer_id in answer to null

--- app/Answer.php
+++ app/Answer.php
@@ -29,9 +29,20 @@ class Answer extends Model
             $answer->question()->increment('answers_count');
         });
         static::deleted(function($answer){
-            $answer->question()->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id == $answer->id)
+            {
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
 
+    }
+
+    function getStatusAttribute()
+    {
+            return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 
     function getDateCreatedAttribute()
