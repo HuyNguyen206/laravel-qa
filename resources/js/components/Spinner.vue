28f735ea75cd926fffbe9ad0c8d01eb7e611c50d
HuyNguyen206 <nguyenlehuyuit@gmail.com>
2021-01-10 16:29:44 +0700

    Add spinner.vue component

---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <div class="m-auto" style="color:gray">
+            <i class="fas fa-spinner fa-spin fa-3x"></i>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Spinner"
+    }
+</script>
+
+<style scoped>
+
+</style>
