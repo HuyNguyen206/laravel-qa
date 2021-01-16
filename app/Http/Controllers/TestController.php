3271485a5f0b22fd38ef3be67696204655bb61b4
HuyNguyen206 <nguyenlehuyuit@gmail.com>
2021-01-16 16:49:50 +0700

    Try to push to heroku

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,8 @@ class TestController extends Controller
     //
     function test()
     {
+
+        dd();
         dump(\Auth::user()->questions);
         dump(\Auth::user()->answers);
         dump($post = \Auth::user()->questions->toBase()->merge(\Auth::user()->answers));
